ab487c8022c4e617c04a755f137307b07861bd28
Aung Ko Man <aungkoman@gmail.com>
2021-10-17 01:14:13 +0630

    api slash end

--- lib/data/constant.dart
+++ lib/data/constant.dart
@@ -4,11 +4,13 @@ import 'package:flutter/material.dart';
 final String appName = "KZN Learning";
 // production server http://13.213.196.157:8000 2021-10-08 Updated
 // development server http://192.168.1.2:8000
-final String backendApiServer = 'http://13.213.196.157:8000';
+// http://127.0.0.1:8000/
+//final String backendApiServer = 'http://13.213.196.157:8000';
+final String backendApiServer = 'http://127.0.0.1:8000';
 final String apiVersion = backendApiServer+'';
 final String loginEndpoint = apiVersion+'/api/token/';
-final String courseEndpoint = apiVersion+'/courses';
-final String subscriptionEndpoint = apiVersion+'/subscriptions';
+final String courseEndpoint = apiVersion+'/courses/';
+final String subscriptionEndpoint = apiVersion+'/subscriptions/';
 
 final String appIconAsset = "assets/images/app_icon.png";
 final String appIconUrl = "https://i.ibb.co/LRZR7DT/app-icon.png";
