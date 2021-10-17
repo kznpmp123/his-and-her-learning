a4f11874607df4f235d90bccb8e8f92871c1f154
Aung Ko Man <aungkoman@gmail.com>
2021-10-17 11:41:25 +0630

    web video player support

--- lib/ui/components/single/lesson_video_player.dart
+++ lib/ui/components/single/lesson_video_player.dart
@@ -1,8 +1,10 @@
 import 'package:better_player/better_player.dart';
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:kzn/data/models/lesson.dart';
 import 'package:kzn/providers/course_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:video_player/video_player.dart';
 
 class LessonVideoPlayer extends StatefulWidget {
   @override
@@ -12,6 +14,10 @@ class LessonVideoPlayer extends StatefulWidget {
 class _LessonVideoPlayerState extends State<LessonVideoPlayer> {
   BetterPlayerController _betterPlayerController;
 
+   VideoPlayerController _controller;
+   Future<void> _initializeVideoPlayerFuture;
+
+  ChewieController chewieController;
   @override
   void initState() {
     // TODO: implement initState
@@ -28,7 +34,45 @@ class _LessonVideoPlayerState extends State<LessonVideoPlayer> {
   void dispose() {
     // TODO: implement dispose
     _betterPlayerController.dispose();
+
+    _controller.dispose();
+    chewieController.dispose();
     super.dispose();
+  }
+
+
+  Widget _newVideoPlayer(){
+    return FutureBuilder(
+      future: _initializeVideoPlayerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // If the VideoPlayerController has finished initialization, use
+          // the data it provides to limit the aspect ratio of the video.
+          chewieController = ChewieController(
+            videoPlayerController: _controller,
+            autoPlay: true,
+            looping: true,
+          );
+          return Chewie(
+            controller: chewieController,
+          );
+          /*
+          return AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            // Use the VideoPlayer widget to display the video.
+            child: VideoPlayer(_controller),
+          );
+
+           */
+        } else {
+          // If the VideoPlayerController is still initializing, show a
+          // loading spinner.
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
   @override
   Widget build(BuildContext context) {
@@ -55,7 +99,36 @@ class _LessonVideoPlayerState extends State<LessonVideoPlayer> {
                   snapshot.data.videoUrl
               );
               _betterPlayerController.setupDataSource(betterPlayerDataSource);
-              return  _videoPlayer();
+              // return  _videoPlayer();
+
+              // snapshot.data.videoUrl
+              // 'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4'
+
+              _controller = VideoPlayerController.network(
+                snapshot.data.videoUrl,
+              );
+
+              // Initialize the controller and store the Future for later use.
+              _initializeVideoPlayerFuture = _controller.initialize();
+              // Use the controller to loop the video.
+              //_controller.setLooping(true);
+              //_controller.play();
+              return _newVideoPlayer();
+
+
+
+              /*
+              final videoPlayerController = VideoPlayerController.network(
+                  'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
+
+              // await videoPlayerController.initialize();
+
+
+
+              //return _newVideoPlayer();
+              // return _chewPlayer(chewieController);
+
+               */
             }
           }
           else if (snapshot.hasError) {
